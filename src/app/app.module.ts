cea6ccb51182b0c07810889909fa6d67425064ff
Vdiwanji123 <vaishnavidiwanji.623@gmail.com>
2021-07-20 20:24:28 +0530

    hi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { RadiobuttonComponent } from './radiobutton/radiobutton.component';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatRadioModule} from '@angular/material/mat-radio-group';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RadiobuttonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,MatButtonToggleModule,MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
